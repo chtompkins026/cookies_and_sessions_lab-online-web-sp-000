23d2b08539d85aa54ee8692738e2db9111633126
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-19 19:21:40 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,11 @@ class ProductsController < ApplicationController
   helper_method :cart
   
   def index
-    end
-  
-    def add
-      cart << params[:product]
-      render :index
-    end
   end
+  
+  def add
+    cart << params[:product]
+    render :index
+  end
+
+end
