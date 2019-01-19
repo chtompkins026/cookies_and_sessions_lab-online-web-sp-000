9bd1e5db6705ceb748412753b9c85195b5d55588
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-19 19:09:56 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
- 
-def index
+  skip_before_action :verify_authenticity_token
+  helper_method :cart
+  
+  def index
+    end
+  
+    def add
+      cart << params[:product]
+      render :index
+    end
   end
-
-  def add
-    cart << params[:product]
-    render :index
-  end
-end
